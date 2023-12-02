9a6fa6fcd83ce5c51b896d5caff8643624d26523
belanovic <goranbelanovic@yahoo.com>
2023-12-02 17:43:52 +0100

    return added to routes functions

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ app.use(function (req, res, next) {
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
+    
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
--- routes/authUserFE.js
+++ routes/authUserFE.js
@@ -7,23 +7,23 @@ const bcrypt = require('bcrypt');
 router.post('/authOneUserFE', async (req, res) => { 
 
     const { error } = validateUserAuth(_.pick(req.body, ['username', 'password']));
-    if (error) res.status(400).send([false, 'validation_error', error.details[0].message]);
+    if (error) return res.status(400).send([false, 'validation_error', error.details[0].message]);
 
     const resultUsername = await UserFrontend.findOne({ username: req.body.username });
-    if (!resultUsername) res.status(400).send([false, 'username_error', `Invalid username or password`]);
+    if (!resultUsername) return res.status(400).send([false, 'username_error', `Invalid username or password`]);
     
     const resultPassword = await bcrypt.compare(req.body.password, resultUsername.password);
-    if (!resultPassword) res.status(400).send([false, 'password_error', `Invalid username or password`]);
+    if (!resultPassword) return res.status(400).send([false, 'password_error', `Invalid username or password`]);
 
     const token = resultUsername.generateToken();
 
     try {
         let msg = [true, 'login_successfull', resultUsername, token];
-        res.send(msg);
+        return res.send(msg);
     }
     catch (err) {
         let msg = ['login_error', err]
-        res.send(msg);
+        return res.send(msg);
     }
 })
 
